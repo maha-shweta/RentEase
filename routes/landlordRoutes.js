f6d125c495b296f71566e2d6764c084d5b7e7f64
Mahashweta Manjari Barua <mahashwetamanjari@gmail.com>
2025-11-24 22:46:32 +0600

    Delete routes directory

--- routes/landlordRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerLandlord, loginLandlord } = require('../controllers/landlordController');  // Import controller functions
-const router = express.Router();
-
-// Landlord Routes
-router.post('/signup', registerLandlord);  // Landlord signup route
-router.post('/login', loginLandlord);  // Landlord login route
-
-module.exports = router;
